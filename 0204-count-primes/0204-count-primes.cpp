19b0dc5c2b89a43f703b62f90e858ffcfdd06f68
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-03 23:47:11 +0530

    Time: 423 ms (6.85%), Space: 12.8 MB (38.45%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // By applying Sieve of Eratosthenes
+        // First we suppose that all numbers are prime
+        vector<bool> prime(n+1,true);
+
+        int count_prime = 0;
+
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                count_prime++;
+            }
+
+            // Multiples of i should be marked false now
+            for(long long j = (long long)i * i; j < n; j += i) {
+                prime[j] = false;
+            }
+        }
+
+        return count_prime;
+    }
+};
