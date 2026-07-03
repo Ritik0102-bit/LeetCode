92785313ed54882310bdc0eef4e499a09c2c818b
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-03 14:30:52 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.47%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1){
+            return arr[0];
+        }
+
+        // Pre-check the boundaries so we never go out of bounds in the loop
+        if (arr[0] != arr[1]) return arr[0];
+        if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+        int left = 1;
+        int right = n - 2;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+                return arr[mid];
+            }
+            // If the elements on the left & right are even 
+            if(mid%2 == 0){
+                if(arr[mid-1]==arr[mid]){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+            // If the elements on the left & right are odd
+            else{
+                if(arr[mid-1]==arr[mid]){
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
