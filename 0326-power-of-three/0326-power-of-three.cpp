a8565a1ab650a0b5e4a67c8839976503b7948e23
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-07 15:37:04 +0530

    Time: 9 ms (9.12%), Space: 8.8 MB (97.83%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1){ // 3^0 == 1
-            return true;
-        }
-
-        if(n<3){
+        if(n <= 0){
             return false;
         }
-        
-        while(n >= 0){
-            if(n==3){
-                cout << n;
-                return true;
-            }
-            if(n%3 != 0){
-                return false;
-            }
-            n/=3;
+
+        // we will keep on dividing our number 
+        while(n%3 == 0){
+            n /= 3;
         }
 
-        return false;
+        // if at last our number become 1 , then it is power of 3
+        return n==1;
     }
 };
