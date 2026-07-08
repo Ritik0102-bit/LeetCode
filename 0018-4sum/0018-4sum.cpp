d942bd7c8440992459b48f16363d8e84d1321ec2
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-08 18:39:46 +0530

    Time: 25 ms (29.75%), Space: 17.4 MB (55.39%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        // We need to sort the array to handle the duplicates
+        sort(arr.begin(),arr.end());
+
+        vector<vector<int>> ans;
+
+        int n = arr.size();
+
+        for(int i=0;i<n;i++){
+            // To Handle Duplicate values
+            if(i > 0 && arr[i]==arr[i-1]){
+                continue;
+            }
+
+            for(int j = i+1 ; j<n;){                
+                int p = j+1;
+                int q = n-1;
+
+                while(p<q){
+                    long long quadrupletSum = (long long)arr[i] + (long long)arr[j] + (long long)arr[p] + (long long)arr[q];
+
+                    if(quadrupletSum == target){
+                        ans.push_back({arr[i],arr[j],arr[p],arr[q]});
+                        p++;
+                        q--;
+
+                        // To Handle Duplicate values
+                        while(p < arr.size() && arr[p]==arr[p-1]){
+                            p++;
+                        }
+                    } 
+                    else if(quadrupletSum < target){
+                        p++;
+                    }
+                    else{
+                        q--;
+                    }
+                }
+
+                j++;
+                // To Handle Duplicate values
+                while(j < arr.size() && arr[j]==arr[j-1]){
+                    j++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
