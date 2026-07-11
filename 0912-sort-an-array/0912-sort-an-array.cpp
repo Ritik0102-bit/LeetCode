3e3f459371ba62311fb035b272e70a57e1dd3118
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-11 14:58:37 +0530

    Time: 55 ms (69.72%), Space: 73.1 MB (66.39%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-    void Merge(vector<int>& arr,int start,int mid,int end){
+    void Merge(vector<int>& arr,int start,int mid,int end,vector<int>& temp){
         int i=start;
         int j=mid+1;
+        int k=start; // pointer for temp array
 
-        vector<int> temp;
         while(i<= mid && j<=end){
             if(arr[i]<arr[j]){
-                temp.push_back(arr[i]);
-                i++;
+                temp[k++] = arr[i++];
             }
             else{
-                temp.push_back(arr[j]);
-                j++;
+                temp[k++] = arr[j++];
             }
         }
         while(i<= mid){
-            temp.push_back(arr[i]);
-            i++;
+            temp[k++] = arr[i++];
         }
         while(j<=end){
-            temp.push_back(arr[j]);
-            j++;
+            temp[k++] = arr[j++];
         }
 
-        int n = temp.size();
-        for(int i=0;i<n;i++){
-            arr[i+start] = temp[i];
+        // Put back the sorted elements into main arr
+        for(int i=start;i<=end;i++){
+            arr[i] = temp[i];
         }
     }
 
-    void MergeSort(vector<int>& arr,int start,int end){
+    void MergeSort(vector<int>& arr,int start,int end,vector<int>& temp){
         if(start < end){
             int mid = start + (end - start)/2;
 
-            MergeSort(arr,start,mid);
-            MergeSort(arr,mid+1,end);
+            MergeSort(arr,start,mid,temp);
+            MergeSort(arr,mid+1,end,temp);
 
-            Merge(arr,start,mid,end);
+            Merge(arr,start,mid,end,temp);
         }
     }
     vector<int> sortArray(vector<int>& arr) {
         int n = arr.size();
+        vector<int> temp(n);
 
-        MergeSort(arr,0,n-1);
+        MergeSort(arr,0,n-1,temp);
 
         return arr;
     }
