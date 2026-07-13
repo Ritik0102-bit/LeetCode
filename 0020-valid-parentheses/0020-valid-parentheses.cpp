d0f438b8547384526f779b9a8c7bc3d55523e883
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-13 14:16:08 +0530

    Time: 1 ms (18.26%), Space: 8.8 MB (89.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        for(int i=0;i<str.size();i++){
+            if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+                s.push(str[i]);
+            }
+            else if(str[i] == ')'  && !s.empty() &&  s.top() == '('){
+                s.pop();
+            }
+            else if(str[i] == ']'  && !s.empty() &&  s.top() == '['){
+                s.pop();
+            }
+            else if(str[i] == '}' && !s.empty() &&  s.top() == '{' ){
+                s.pop();
+            }
+            else{
+                return false;
+            }
+        }
+
+        return s.empty();
+    }
+};
