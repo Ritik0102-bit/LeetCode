0809fee4820105c957b4cea1dbc2723829796d14
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-13 21:36:13 +0530

    Time: 0 ms (100%), Space: 10 MB (89.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    // Memoization DP
-    int helper(vector<int>& arr,int idx,vector<int>& dp){
-        if(idx >= arr.size()){
-            return 0;
-        }
-
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-
-        // Not Included the house
-        int choice1 = helper(arr,idx+1,dp);
-        // Included the house
-        int choice2 = arr[idx] + helper(arr,idx+2,dp);
-
-        return dp[idx] = max(choice1,choice2);
-    }
-    
+    // Space Optimised DP
     int rob(vector<int>& arr) {
         int n = arr.size();
 
-        vector<int> dp(n+1,-1);
+        if(n==1) return arr[0];
 
-        return helper(arr,0,dp);
+        int prev1 = arr[0];
+        int prev2 = max(arr[0],arr[1]);
+
+        int result = prev2;
+
+        for(int i=2;i<n;i++){
+            result = max(prev1 + arr[i] , prev2);
+            prev1 = prev2;
+            prev2 = result;
+        }
+
+        return prev2;
     }
 };
