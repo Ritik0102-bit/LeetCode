ee654e8a910cef5131eed79991560569c82489f0
Ritik Rana <Ritikranaagra82@gmail.com>
2026-07-22 16:31:39 +0530

    Time: 86 ms (13.7%), Space: 113.3 MB (9.63%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& arr) {
+        unordered_map<int,int> m;
+
+        int n = arr.size();
+
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        } 
+
+        for(int i=0;i<n;i++){
+            if(m[arr[i]] > 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
