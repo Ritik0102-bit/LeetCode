9436017cf66189ba8ca5da41d5a63be74ebd9858
Ritik Rana <Ritikranaagra82@gmail.com>
2026-08-18 11:45:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.72%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     bool checkPerfectNumber(int n) {
-        int sum = 0;
-        for(int i=1;i<n;i++){
+        // Base Case
+        if(n <= 1){
+            return false;
+        }
+
+        // 1 is divisor for all the numbers
+        int sum = 1;
+        for(int i=2 ; i*i <= n ; i++){
+            // Golden Rule : Factors always comes in pairs 
+            // i & n/i
             if(n % i == 0){
                 sum += i;
+                // if number is perfect square like 36 , so we just add 6 
+                if(i*i != n){
+                    sum += (n/i);
+                }
             }
             if(sum > n){
                 return false;
