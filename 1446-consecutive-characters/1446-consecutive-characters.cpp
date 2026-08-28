f3b79b65c91994e78c00066a6d43a1801a7d04e8
Ritik Rana <Ritikranaagra82@gmail.com>
2026-08-28 22:17:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.92%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int ans = 1;
+        int c = 1;
+
+        for(int i=0;i<s.size() - 1;i++){
+            if(s[i] == s[i+1]){
+                c++;
+                ans = max(ans,c);
+            }
+            else{
+                c = 1;
+            }
+        }
+
+        return ans;
+    }
+};
